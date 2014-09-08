cb7626300c58527793924bd0d6839d49c91715fc
Christian Dancke Tuen <mail@christiandt.com>
2014-09-08 16:01:43 +0200

    finished squeeze, added hash function example, fixed misc errors

--- spritz.py
+++ spritz.py
@@ -7,6 +7,7 @@ N = 0
 
 def initialize_state(N_input):
     global S, D, N
+    global i, j, k, z, w, a
     i = j = k = z = a = 0
     w = 1
     N = N_input
@@ -21,20 +22,21 @@ def high(b):
 
 def update():
     global S, i, j, k
-    i = i + w
-    j = k + S[j+S[i]]
-    k = i + k + S[j]
+    i = (i + w) % N
+    j = (k + S[(j + S[i]) % N]) % N
+    k = (i + k + S[j]) % N
     S[i], S[j] = S[j], S[i]
 
 def whip(r):
+    global w
     for v in range(r):
         update()
     w = w + 1
     while gcd(w, N) != 1:
-        w = w + 1
+        w = (w + 1) % N
 
 def crush():
-    for v in range(math.floor(N/2)-1):
+    for v in range(int(math.floor(N/2)-1)):
         if S[v] > S[N-1-v]:
             S[v], S[N-1-v] = S[N-1-v], S[v]
 
@@ -49,10 +51,10 @@ def shuffle():
 
 def absorb_nibble(x):
     global S, a
-    if a = int(math.floor(N/2)):
+    if a == int(math.floor(N/2)):
         shuffle()
-    S[a], S[math.floor(N/2)+x] = S[math.floor(N/2)+x], S[a]
-    a = a + 1
+    S[a], S[int(math.floor(N/2)+x)] = S[int(math.floor(N/2)+x)], S[a]
+    a = (a + 1) % N
 
 def absorb_byte(b):
     absorb_nibble(low(b))
@@ -63,12 +65,14 @@ def absorb(I):
         absorb_byte(I[v])
 
 def absorb_stop():
-    if a = int(math.floor(N/2)):
+    global a
+    if a == int(math.floor(N/2)):
         shuffle()
-    a = a + 1
+    a = (a + 1) % N
 
 def output():
-    z = S[j + S[i + S[z + k]]]
+    global z
+    z = S[(j + S[(i + S[(z + k) % N]) % N]) % N]
     return z
 
 def drip():
@@ -78,13 +82,25 @@ def drip():
     return output()
 
 def squeeze(r):
-    pass
+    if a > 0:
+        shuffle()
+    P = []
+    for v in range(r):
+        P.append(drip())
+    return P
+
+
+def ascii_array(string):
+    key = []
+    for char in string:
+        key.append(ord(char))
+    return key
 
 
 initialize_state(256)
-print S
-print D
-print N
+absorb(ascii_array("abc"))
+absorb(ascii_array("def"))
+print squeeze(32)
 
-print low(50)
-print high(50)
+absorb(ascii_array("ghi"))
+print squeeze(1000)
