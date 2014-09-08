f30cb35fc82d2510e5b9648c20f44c380731818c
Christian Dancke Tuen <mail@christiandt.com>
2014-09-08 13:53:09 +0200

    Added the complete left branch of tree diagram, not yet tested.

--- spritz.py
+++ spritz.py
@@ -1,33 +1,72 @@
 import math
+from fractions import gcd
 
 S = []
 D = 0
 N = 0
 
 def initialize_state(N_input):
-	global S, D, N
-	i = j = k = z = a = 0
-	w = 1
-	N = N_input
-	D = int(math.ceil(math.sqrt(N)))
-	for v in range(N):
-		S.append(v)
+    global S, D, N
+    i = j = k = z = a = 0
+    w = 1
+    N = N_input
+    D = int(math.ceil(math.sqrt(N)))
+    S = range(N)
 
 def low(b):
-	return b % D
+    return b % D
 
-def high(n):
-	return int(math.floor(b/D))
+def high(b):
+    return int(math.floor(b/D))
+
+def update():
+    global S, i, j, k
+    i = i + w
+    j = k + S[j+S[i]]
+    k = i + k + S[j]
+    S[i], S[j] = S[j], S[i]
+
+def whip(r):
+    for v in range(r):
+        update()
+    w = w + 1
+    while gcd(w, N) != 1:
+        w = w + 1
+
+def crush():
+    for v in range(math.floor(N/2)-1):
+        if S[v] > S[N-1-v]:
+            S[v], S[N-1-v] = S[N-1-v], S[v]
+
+def shuffle():
+    global a
+    whip(2*N)
+    crush()
+    whip(2*N)
+    crush()
+    whip(2*N)
+    a = 0
+
+def absorb_nibble(x):
+    global S, a
+    if a = int(math.floor(N/2)):
+        shuffle()
+    S[a], S[math.floor(N/2)+x] = S[math.floor(N/2)+x], S[a]
+    a = a + 1
 
 def absorb_byte(b):
-	absorb_nibble()
+    absorb_nibble(low(b))
+    absorb_nibble(high(b))
 
 def absorb(I):
-	for v in range(len(I)):
-		absorb_byte(I[v])
+    for v in range(len(I)):
+        absorb_byte(I[v])
 
 
 initialize_state(256)
 print S
 print D
 print N
+
+print low(50)
+print high(50)
