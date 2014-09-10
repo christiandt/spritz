03a22d8f094279528ffa22ab20ae185d13a8bd87
Christian Dancke Tuen <mail@christiandt.com>
2014-09-10 20:02:41 +0200

    Added example enc/dec

--- RC4/rc4.py
+++ RC4/rc4.py
@@ -1,3 +1,5 @@
+from re import findall
+
 S = []
 i = 0
 j = 0
@@ -5,6 +7,8 @@ j = 0
 def ksa(keystring):
 	global S
 	key = []
+	S = []
+
 	for char in keystring:
 		key.append(ord(char))
 
@@ -18,18 +22,39 @@ def ksa(keystring):
 
 
 def prg():
-	global S
-	global i
-	global j
+	global S, i, j
 
-	i = i + 1
-	j = j + S[i]
+	i = (i + 1) % 256
+	j = (j + S[i]) % 256
 	S[i], S[j] = S[j], S[i]
-	z = S[S[i] + S[j]]
+	z = S[(S[i] + S[j]) % 256]
 	return z
 
+def encrypt(plaintext):
+	ciphertext = ""
+	for char in plaintext:
+		ciphertext += '{0:02x}'.format(ord(char) ^ prg()).upper()
+	return ciphertext
+
+def decrypt(ciphertext):
+	plaintext = ""
+	for hex_num in findall('..', ciphertext):
+		plaintext += chr(int(hex_num, 16) ^ prg())
+	return plaintext
 
 
 ksa('Key')
-print S
-print prg()
+ciph = encrypt('Plaintext')
+print ciph
+
+i = j = 0
+ksa('Key')
+plain = decrypt(ciph)
+print plain
+
+#ksa('Wiki')
+#ciph = encrypt('pedia')
+#print ciph
+#ksa('Wiki')
+#plain = decrypt(ciph)
+#print plain
