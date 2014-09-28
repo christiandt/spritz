0cc80362698cea7c8270f6fc76602b3181d7cb5d
Christian D Tuen <christiandt@gmail.com>
2014-09-29 00:45:45 +0200

    Added comments to check code vs. paper

--- spritz.py
+++ spritz.py
@@ -4,84 +4,84 @@ from fractions import gcd
 class Spritz():
 
     def initialize_state(self, N_input):
-        self.i = 0
+        self.i = 0  # i=j=k=z=a=0
         self.j = 0
         self.k = 0
         self.z = 0
         self.a = 0
-        self.w = 1
+        self.w = 1  # w=1
         self.N = N_input
         self.D = int(math.ceil(math.sqrt(self.N)))
-        self.S = range(self.N)
+        self.S = range(self.N)  #S[v] = v
 
     def low(self, b):
-        return b % self.D
+        return b % self.D   # b mod D
 
     def high(self, b):
-        return int(math.floor(b/self.D))
+        return int(math.floor(b/self.D))    # ⌊b/D⌋
 
     def update(self):
-        self.i = (self.i + self.w) % self.N
-        self.j = (self.k + self.S[(self.j + self.S[self.i]) % self.N]) % self.N
-        self.k = (self.i + self.k + self.S[self.j]) % self.N
-        self.S[self.i], self.S[self.j] = self.S[self.j], self.S[self.i]
+        self.i = (self.i + self.w) % self.N     # i=i+w
+        self.j = (self.k + self.S[(self.j + self.S[self.i]) % self.N]) % self.N     # j=k+S[j+S[i]]
+        self.k = (self.i + self.k + self.S[self.j]) % self.N    # k=i+k+S[j]
+        self.S[self.i], self.S[self.j] = self.S[self.j], self.S[self.i]     # Swap(S[i], S[j])
 
     def whip(self, r):
-        for v in range(r):
-            self.update()
-        self.w = self.w + 1
-        while gcd(self.w, self.N) != 1:
+        for v in range(r):  # for v=0 to r−1
+            self.update()   # Update()
+        self.w = self.w + 1 # do w=w+1
+        while gcd(self.w, self.N) != 1: # until gcd(w,N) = 1
             self.w = (self.w + 1) % self.N
 
     def crush(self):
-        for v in range(int(math.floor(self.N/2)-1)):
-            if self.S[v] > self.S[self.N-1-v]:
-                self.S[v], self.S[self.N-1-v] = self.S[self.N-1-v], self.S[v]
+        for v in range(int(math.floor(self.N/2)-1)):    # for v=0 to ⌊N/2⌋−1
+            if self.S[v] > self.S[self.N-1-v]:  # if S[v]>S[N−1−v]
+                self.S[v], self.S[self.N-1-v] = self.S[self.N-1-v], self.S[v]   # Swap(S[v], S[N−1−v])
 
     def shuffle(self):
-        self.whip(2*self.N)
-        self.crush()
-        self.whip(2*self.N)
-        self.crush()
-        self.whip(2*self.N)
-        self.a = 0
+        self.whip(2*self.N) # Whip(2N)
+        self.crush()        # Crush()
+        self.whip(2*self.N) # Whip(2N)
+        self.crush()        # Crush()
+        self.whip(2*self.N) # Whip(2N)
+        self.a = 0          # a=0
 
     def absorb_nibble(self, x):
-        if self.a == int(math.floor(self.N/2)):
-            self.shuffle()
-        self.S[self.a], self.S[int(math.floor(self.N/2)+x)] = self.S[int(math.floor(self.N/2)+x)], self.S[self.a]
-        self.a += 1 % self.N
+        if self.a == int(math.floor(self.N/2)):     # if a=⌊N/2⌋
+            self.shuffle()  # Shuffle()
+        self.S[self.a], self.S[int(math.floor(self.N/2)+x)] = self.S[int(math.floor(self.N/2)+x)], self.S[self.a]   # Swap(S[a],S[⌊N/2⌋+x])
+        self.a += 1 % self.N    # a=a+1
 
     def absorb_byte(self, b):
-        self.absorb_nibble(self.low(b))
-        self.absorb_nibble(self.high(b))
+        self.absorb_nibble(self.low(b))     # AbsorbNibble(low(b))
+        self.absorb_nibble(self.high(b))    # AbsorbNibble(high(b))
 
     def absorb(self, I):
-        for v in I:
-            self.absorb_byte(v)
+        for v in I:     # for v=0 to I.length−1
+            self.absorb_byte(v)     # AbsorbByte(I[v])
 
     def absorb_stop(self):
-        if self.a == int(math.floor(self.N/2)):
-            self.shuffle()
-        self.a += 1 % self.N
+        if self.a == int(math.floor(self.N/2)):     # if a=⌊N/2⌋
+            self.shuffle()  # Shuffle()
+        self.a += 1 % self.N    # a=a+1
 
     def output(self):
-        self.z = self.S[(self.j + self.S[(self.i + self.S[(self.z + self.k) % self.N]) % self.N]) % self.N]
-        return self.z
+        self.z = self.S[(self.j + self.S[(self.i + self.S[(self.z + self.k) % self.N]) % self.N]) % self.N]     # z = S[j+S[i+S[z+k]]]
+        return self.z   # return z
 
     def drip(self):
-        if self.a > 0:
-            self.shuffle()
-        self.update()
-        return self.output()
+        if self.a > 0:  # if a>0
+            self.shuffle() # Shuffle()
+        self.update()   # Update()
+        return self.output()    # return Output()
 
     def squeeze(self, r):
-        if self.a > 0:
-            self.shuffle()
-        P = []
-        for v in range(r):
-            P.append(self.drip())
-        return P
+        if self.a > 0:  # if a>0
+            self.shuffle()  # Shuffle()
+        P = []  # P = Array.New(r)
+        for v in range(r):  # for v=0 to r−1
+            P.append(self.drip())   # P[v] = Drip()
+        return P    # return P
 
     def print_variables(self):
         print "i: " + hex(self.i)
@@ -118,4 +118,4 @@ class Spritz():
 
 #s = Spritz()
 #print s.hash(256, "ABC", 32)
-#s.print_variables()      # test-print
+#s.print_variables()
