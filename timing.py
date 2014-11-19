3b6eef333289d5afdbc7643ef93dcaec91307341
Christian Dancke Tuen <mail@christiandt.com>
2014-11-19 01:27:43 +0100

    The timing-graph now tests both crush functions and graphs them in the same graph

--- timing.py
+++ timing.py
@@ -5,7 +5,8 @@ from string_creator import String_Creator
 
 s = Spritz()
 string_maker = String_Creator()
-values = dict()
+key = ""
+runs = 1000
 
 def keysize():
 	print "%i bit key" % ((len(key)/3)*8)
@@ -13,25 +14,37 @@ def keysize():
 def timingtest():
 	s.key_setup(key)
 
+def generate_data(alt):
+	global key
+	values = dict()
+	s.alt = alt
+	key = s.int_string("keykeyke")
+	#key = s.int_string(string_maker.create_string(8))
+	keysize()
+	for i in range(runs):
+		time_result = round(timeit("timingtest()", setup="from __main__ import timingtest", number=100) * 1000, 2)
+		#print time_result
+		if time_result in values:
+			values[time_result] += (1.0/runs)
+		else:
+			values[time_result] = (1.0/runs)
 
-key = s.int_string("keykeyke")
-#key = s.int_string(string_maker.create_string(8))
-keysize()
-for i in range(500):
-	time_result = round(timeit("timingtest()", setup="from __main__ import timingtest", number=100) * 1000, 2)
-	#print time_result
-	if time_result in values:
-		values[time_result] += 1
-	else:
-		values[time_result] = 1
 
+	x_plot = []
+	y_plot = []
+	for value in sorted(values):
+		x_plot.append(value)
+		y_plot.append(values[value])
+	return x_plot, y_plot
 
-x_plot = []
-y_plot = []
-for value in sorted(values):
-	x_plot.append(value)
-	y_plot.append(values[value])
+x_alt, y_alt = generate_data(True)
+x_data, y_data = generate_data(False)
 
-plt.plot(x_plot, y_plot, 'bo-')
-plt.axis([6, 8, 0, 40])
+plt.plot(x_data, y_data, 'b', label='Original')
+plt.plot(x_alt, y_alt, 'r', label='Prevent')
+plt.legend(loc='upper right')
+plt.axis([4.7, 5.2, 0, 0.13])
+plt.title('Timing')
+plt.xlabel('ms')
+plt.ylabel('percent')
 plt.show()
